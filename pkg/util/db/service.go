96ff27bd339e961d5011f77f5533edb31c182ee8
Nguyen Nguyen <khanhnguyen1411@gmail.com>
2023-05-20 13:48:15 +0700

    add sqlite

--- pkg/util/db/service.go
+++ pkg/util/db/service.go
@@ -3,25 +3,28 @@ package dbutil
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 // New creates new database connection to the database server
-func New(dialect, dbPsn string, cfg *gorm.Config) (*gorm.DB, error) {
-	db := new(gorm.DB)
+func New(dialect, dbPsn string, cfg *gorm.Config) (db *gorm.DB, err error) {
 	switch dialect {
 	case "mysql":
-		db, err := gorm.Open(mysql.Open(dbPsn), cfg)
+		db, err = gorm.Open(mysql.Open(dbPsn), cfg)
 		if err != nil {
 			return nil, err
 		}
-		return db, nil
 	case "postgres":
-		db, err := gorm.Open(postgres.Open(dbPsn), cfg)
+		db, err = gorm.Open(postgres.Open(dbPsn), cfg)
 		if err != nil {
 			return nil, err
 		}
-		return db, nil
+	case "sqlite3":
+		db, err = gorm.Open(sqlite.Open(dbPsn), cfg)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	return db, nil
--- pkg/util/db/service_test.go
+++ pkg/util/db/service_test.go
@@ -16,20 +16,20 @@ func TestNew(t *testing.T) {
 	tests := []struct {
 		name    string
 		args    args
-		want    *gorm.DB
+		wantDb  *gorm.DB
 		wantErr bool
 	}{
 		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := New(tt.args.dialect, tt.args.dbPsn, tt.args.cfg)
+			gotDb, err := New(tt.args.dialect, tt.args.dbPsn, tt.args.cfg)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
+			if !reflect.DeepEqual(gotDb, tt.wantDb) {
+				t.Errorf("New() = %v, want %v", gotDb, tt.wantDb)
 			}
 		})
 	}
